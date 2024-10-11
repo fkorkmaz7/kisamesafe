43b0cbbeb401094035675d879babbf907312db7c
fkorkmaz7 <furkank1024@gmail.com>
2024-10-11 18:59:18 +0300

    main branchine aktarım

--- models/alici.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const AliciSchema = new mongoose.Schema({
-    ad_soyad : {type: String},
-    butce : {type: Number},
-    parola : {type: Number},
-    tel_no: {type: Number},
-})
-const Alici = mongoose.model("Alici", AliciSchema);
-module.exports= Alici;
--- models/fatura.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const FaturaSchema = new mongoose.Schema({
-    alici : {type: String},
-    fiyat : {type: Number},
-    urun : {type: String},
-    tarih: {type: Number},
-})
-const Fatura = mongoose.model("Fatura", FaturaSchema);
-module.exports= Fatura;
