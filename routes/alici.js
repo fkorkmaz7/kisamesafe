e960994e87b18e4f0b66b1ae4c2fee3e0a89a7a5
Sinan Kalender <sinankalender06@gmail.com>
2024-10-15 18:50:52 +0300

    Sinan hafta sonu çalışma

--- routes/alici.js
+++ routes/alici.js
@@ -2,25 +2,14 @@ const express = require('express');
 const Alici = require('../models/alici');
 const { signIn,signUp } = require('../services/auth');
 const {login} = require('../controllers/auth')
+const aliciController = require(`../controllers/alici`)
 const router = express.Router();
 
 
-router.route('/').get(async (req, res, next) => {
-    const alicilar = await Alici.find();
-    return res.status(200).send(alicilar);
-})
-
-router.route('/').post(async (req,res,next) => {
-    const yeni_alici = await Alici.create(req.body);
-    res.send(yeni_alici);
-});
-
-router.route('/').delete(async (req, res, next) => {
-    const { id } = req.params;
-    const alici = await Alici.findByIdAndDelete(id);
-    return res.status(200).send(alici);
-})
-
+router.route(`/`).get(aliciController.getAlicilar)
+router.route(`/`).post(aliciController.createAlici)
+router.route(`/:`).delete(aliciController.deleteAlici)
+router.route(`/`).get(aliciController.getAlicilar)
 router.route('/logIn').post(login);
 
 module.exports = router;
--- routes/araba.js
+++ routes/araba.js
@@ -1,35 +1,10 @@
 const express = require('express');
-const Araba = require('../models/araba');
 const router = express.Router();
+const arabaController = require('../controllers/araba');
 
-
-router.route('/').get(async (req, res, next) => {
-    const arabalar = await Araba.find();
-    return res.status(200).send(arabalar);
-})
-
-router.route('/').post(async (req,res,next) => {
-    const yeni_araba = await Araba.create(req.body);
-    res.send(yeni_araba);
-});
-
-router.route('/:id').delete(async (req, res, next) => {
-    const { id } = req.params;
-    const araba = await Araba.findByIdAndDelete(id);
-    return res.status(200).send(araba);
-})
-
-router.route('/:id').put(async (req, res, next) => {
-    const { id } = req.params;
-    const { marka, plaka, yil, fiyat, kilometre } = req.body
-    const araba = await Araba.findByIdAndUpdate(id, {
-        marka,
-        plaka,
-        yil,
-        fiyat,
-        kilometre
-    }, { new: true });
-    return res.status(200).send(araba);
-});
+router.route('/get').get(arabaController.getArabalar);
+router.route('/create').post(arabaController.createAraba);
+router.route('/:del').delete(arabaController.deleteAraba);
+router.route('/:update').put(arabaController.updateAraba);
 
 module.exports = router;
