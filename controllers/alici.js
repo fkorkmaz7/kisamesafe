78525f69d6ca53727debb003a7550d412d36b5c7
ebrarkesici <ebrarkesici@gmail.com>
2024-10-15 19:02:30 +0300

    controllers'a alici ve araba eklendi

---
+++ controllers/alici.js
@@ -0,0 +1,18 @@
+const Alici = require(`../models/alici`);
+const getAlicilar = async (req, res, next) => {
+    const alicilar = await Alici.find();
+    return res.status(200).send(alicilar);
+}
+const createAlici = async (req, res, next) => {
+    const yeni_alici = await Alici.create(req.body);
+    return res.send(yeni_alici)
+};
+const deleteAlici = async (req, res, next) => {
+    const alici = await Alici.findbyIdAndDelete(id);
+    return res.status(200).send(alici);
+};
+module.exports = {
+    getAlicilar,
+    createAlici,
+    deleteAlici
+};
---
+++ controllers/araba.js
@@ -0,0 +1,32 @@
+const Araba = require('../models/araba');
+const getArabalar = async (req, res, next) => {
+    const arabalar = await Araba.find();
+    return res.status(200).send(arabalar);
+};
+const createAraba = async (req, res, next) => {
+    const yeni_araba = await Araba.create(req.body);
+    return res.send(yeni_araba);
+};
+const deleteAraba = async (req, res, next) => {
+    const { id } = req.params;
+    const araba = await Araba.findByIdAndDelete(id);
+    return res.status(200).send(araba);
+};
+const updateAraba = async (req, res, next) => {
+    const { id } = req.params;
+    const { marka, plaka, yil, fiyat, kilometre } = req.body;
+    const araba = await Araba.findByIdAndUpdate(id, {
+        marka,
+        plaka,
+        yil,
+        fiyat,
+        kilometre
+    }, { new: true });
+    return res.status(200).send(araba);
+};
+module.exports = {
+    getArabalar,
+    createAraba,
+    deleteAraba,
+    updateAraba,
+};
