fa7dca05eeb6d1e9f59c1061ed18f0d796573ed5
ebrarkesici <ebrarkesici@gmail.com>
2024-10-15 19:24:31 +0300

    son dokunuslar

--- controllers/alici.js
+++ controllers/alici.js
@@ -1,40 +1,71 @@
-const Alici = require(`../models/alici`);
-<<<<<<< HEAD
+const Alici = require('../models/alici');
+
+// Fetch all Alici
 const getAlicilar = async (req, res, next) => {
-    const alicilar = await Alici.find();
-    return res.status(200).send(alicilar);
-}
+    try {
+        const alicilar = await Alici.find();
+        return res.status(200).json(alicilar);
+    } catch (error) {
+        next(error); // Pass error to error-handling middleware
+    }
+};
+
+// Fetch a single Alici by ID
+const getByIdAlici = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const alici = await Alici.findById(id);
+        if (!alici) {
+            return res.status(404).json({ message: 'Alici not found' });
+        }
+        return res.status(200).json(alici);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Create a new Alici
 const createAlici = async (req, res, next) => {
-    const yeni_alici = await Alici.create(req.body);
-    return res.send(yeni_alici)
+    try {
+        const yeni_alici = await Alici.create(req.body);
+        return res.status(201).json(yeni_alici); // 201 status for resource creation
+    } catch (error) {
+        next(error);
+    }
 };
+
+// Update an Alici by ID
+const updateAlici = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const alici = await Alici.findByIdAndUpdate(id, req.body, { new: true });
+        if (!alici) {
+            return res.status(404).json({ message: 'Alici not found' });
+        }
+        return res.status(200).json(alici);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Delete an Alici by ID
 const deleteAlici = async (req, res, next) => {
-    const alici = await Alici.findbyIdAndDelete(id);
-    return res.status(200).send(alici);
-};
-=======
-const getAlicilar = async (req,res,next) => {
-    const alicilar = await Alici.find();
-    return res.status(200).send(alicilar);
-}
-
-const createAlici = async (req,res,next) => {
-    const yeni_alici = await Alici.create(req.body);
-    return res.send(yeni_alici)
+    try {
+        const { id } = req.params;
+        const alici = await Alici.findByIdAndDelete(id);
+        if (!alici) {
+            return res.status(404).json({ message: 'Alici not found' });
+        }
+        return res.status(200).json({ message: 'Alici deleted' });
+    } catch (error) {
+        next(error);
+    }
 };
 
-const deleteAlici = async (req,res,next) => {
-    const alici = await Alici.findbyIdAndDelete(id);
-    return res.status(200).send(alici);
-};
-
->>>>>>> e960994e87b18e4f0b66b1ae4c2fee3e0a89a7a5
 module.exports = {
     getAlicilar,
+    getByIdAlici,
     createAlici,
+    updateAlici,
     deleteAlici
-<<<<<<< HEAD
-=======
-
->>>>>>> e960994e87b18e4f0b66b1ae4c2fee3e0a89a7a5
 };
--- controllers/araba.js
+++ controllers/araba.js
@@ -1,44 +1,71 @@
 const Araba = require('../models/araba');
-<<<<<<< HEAD
-=======
 
->>>>>>> e960994e87b18e4f0b66b1ae4c2fee3e0a89a7a5
+// Fetch all Arabalar (cars)
 const getArabalar = async (req, res, next) => {
-    const arabalar = await Araba.find();
-    return res.status(200).send(arabalar);
+    try {
+        const arabalar = await Araba.find();
+        return res.status(200).json(arabalar);
+    } catch (error) {
+        next(error); // Pass error to error-handling middleware
+    }
 };
+
+// Fetch a single Araba by ID
+const getByIdAraba = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const araba = await Araba.findById(id);
+        if (!araba) {
+            return res.status(404).json({ message: 'Araba not found' });
+        }
+        return res.status(200).json(araba);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// Create a new Araba
 const createAraba = async (req, res, next) => {
-    const yeni_araba = await Araba.create(req.body);
-    return res.send(yeni_araba);
+    try {
+        const yeni_araba = await Araba.create(req.body);
+        return res.status(201).json(yeni_araba); // 201 status for resource creation
+    } catch (error) {
+        next(error);
+    }
 };
-const deleteAraba = async (req, res, next) => {
-    const { id } = req.params;
-    const araba = await Araba.findByIdAndDelete(id);
-    return res.status(200).send(araba);
-};
-<<<<<<< HEAD
-=======
 
->>>>>>> e960994e87b18e4f0b66b1ae4c2fee3e0a89a7a5
+// Update an existing Araba by ID
 const updateAraba = async (req, res, next) => {
-    const { id } = req.params;
-    const { marka, plaka, yil, fiyat, kilometre } = req.body;
-    const araba = await Araba.findByIdAndUpdate(id, {
-        marka,
-        plaka,
-        yil,
-        fiyat,
-        kilometre
-    }, { new: true });
-    return res.status(200).send(araba);
+    try {
+        const { id } = req.params;
+        const araba = await Araba.findByIdAndUpdate(id, req.body, { new: true });
+        if (!araba) {
+            return res.status(404).json({ message: 'Araba not found' });
+        }
+        return res.status(200).json(araba);
+    } catch (error) {
+        next(error);
+    }
 };
-<<<<<<< HEAD
-=======
 
->>>>>>> e960994e87b18e4f0b66b1ae4c2fee3e0a89a7a5
+// Delete an Araba by ID
+const deleteAraba = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const araba = await Araba.findByIdAndDelete(id);
+        if (!araba) {
+            return res.status(404).json({ message: 'Araba not found' });
+        }
+        return res.status(200).json({ message: 'Araba deleted' });
+    } catch (error) {
+        next(error);
+    }
+};
+
 module.exports = {
     getArabalar,
+    getByIdAraba,
     createAraba,
-    deleteAraba,
     updateAraba,
+    deleteAraba
 };
